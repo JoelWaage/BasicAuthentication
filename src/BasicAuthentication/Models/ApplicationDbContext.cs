31dab3e327c8007682bad0cf9b3845246cd7032f
Joel <joeltwaage@gmail.com>
2016-09-27 14:33:06 -0700

    Add Folder and JSON for testing

--- src/BasicAuthentication/Models/ApplicationDbContext.cs
+++ src/BasicAuthentication/Models/ApplicationDbContext.cs
@@ -9,14 +9,21 @@ namespace BasicAuthentication.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BasicAuthentication;integrated security=True");
+        }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
+      
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
-        public DbSet<Item> Items { get; set; }
+       
     }
 }
